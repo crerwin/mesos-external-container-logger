e8c25188aa247efc37704de93f657d8a29d3727d
justin.r.lee@gmail.com <justin.r.lee@gmail.com>
2018-03-21 11:27:55 +0100

    Update compile.sh for 1.4.1, add cmake/cmake3 handling

--- compile.sh
+++ compile.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-cmake -DWITH_MESOS=$PWD/mesos-install -DMESOS_SRC_DIR=$PWD/mesos-1.4.0
+MESOS_VERSION=1.4.1
+
+cmake -DWITH_MESOS=$PWD/mesos-install -DMESOS_SRC_DIR=$PWD/mesos-${MESOS_VERSION} || cmake3 -DWITH_MESOS=$PWD/mesos-install -DMESOS_SRC_DIR=$PWD/mesos-${MESOS_VERSION}
 make -j 6 V=0
