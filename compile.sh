c449e86f10ab7c85b4e9d0b250e684cf3aaea7a6
Justin Lee <jlee@mesosphere.com>
2018-04-03 22:16:23 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-MESOS_VERSION=1.4.2
+MESOS_VERSION=1.4.1
 
 cmake -DWITH_MESOS=$PWD/mesos-install -DMESOS_SRC_DIR=$PWD/mesos-${MESOS_VERSION} || cmake3 -DWITH_MESOS=$PWD/mesos-install -DMESOS_SRC_DIR=$PWD/mesos-${MESOS_VERSION}
 make -j 6 V=0
